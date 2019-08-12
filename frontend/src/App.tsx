49bd38f35f086e191b867afcd98c2fbcad4bd32f
sakata1222 <sakata1222@gmail.com>
2019-08-12 18:09:07 +0900

    feat:implement frontend(not bundled to spring boot)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import RendaringArea from "./RendaringArea";
 
 const App: React.FC = () => {
+  const [inputText, setInputText] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header has-background-warning">
+        <div className="title is-3 has-text-white-ter has-text-weight-bold">PlantUML Viewer</div>
       </header>
+      <div className="App-main">
+        <div className="input-area">
+          <textarea
+            className="box"
+            value={inputText}
+            placeholder="Bob -> Alice: Hello"
+            onChange={(event) => setInputText(event.target.value)}
+          />
+        </div>
+        <div className="rendring-area">
+          <RendaringArea plantUml={inputText}></RendaringArea>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/RendaringArea.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './RendaringArea.scss';
+import { debounce } from "lodash";
+
+interface IProps {
+    readonly plantUml: string;
+}
+
+const RendaringArea: React.FC<IProps> = (props: IProps) => {
+    const [isLoading, setLoading] = useState(true);
+    const [svgPath, setSvgPath] = useState("");
+    // hack to use debounce in a React function component
+    const umlTxt = useRef("");
+    umlTxt.current = props.plantUml;
+    const rendaring = useRef(debounce(() => {
+        setLoading(true)
+        fetch("svg", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+            },
+            body: JSON.stringify({
+                plantUml: "@startuml\n" + umlTxt.current + "\n@enduml"
+            })
+        })
+            .then(resposne => resposne.json())
+            .then(json => {
+                setSvgPath(json.rawDataPath)
+                setLoading(false)
+            })
+    }, 1000)).current;
+
+    useEffect(rendaring, [props.plantUml]);
+
+    if (isLoading) {
+        return <div></div>
+    } else {
+        return <img src={svgPath} alt="Unable to display SVG" />
+    }
+}
+
+export default RendaringArea;
